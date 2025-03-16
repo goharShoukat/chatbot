09b26cc2dcda294e0dee3091903c2aa083215619
Gohar Shoukat <25367760+goharShoukat@users.noreply.github.com>
2025-03-16 09:50:54 +0000

    feat: created a working fe chatbot interface

--- frontend/app/interface.py
+++ frontend/app/interface.py
@@ -5,12 +5,24 @@ import json
 url = "http://127.0.0.1:8000/chat/stream"
 
 
-def echo(text, request: gr.Request):
-    if request:
-        payload = {"query": text}
-        foo = requests.post(url, data=json.dumps(payload), stream=False)
-        bar = foo.content.decode("utf-8")
-        return bar
+def call_llms(text):
+    payload = {"query": text}
+    foo = requests.post(url, data=json.dumps(payload), stream=False)
+    bar = foo.content.decode("utf-8")
+    return bar
 
 
-io = gr.Interface(echo, "textbox", "textbox").launch()
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot(type="messages")
+    msg = gr.Textbox()
+    clear = gr.ClearButton([msg, chatbot])
+
+    def respond(message, chat_history):
+        response = call_llms(message)
+        chat_history.append({"role": "user", "content": message})
+        chat_history.append({"role": "assistant", "content": response})
+
+        return "", chat_history
+
+    msg.submit(respond, [msg, chatbot], [msg, chatbot])
+demo.launch()
